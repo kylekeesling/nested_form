37dced6d41008ac11f7b792ddf8d2d44993a56c5
Kyle Keesling <kyle.keesling@gmail.com>
2011-11-20 17:39:01 -0500

    Trying again

--- lib/nested_form/builder_mixin.rb
+++ lib/nested_form/builder_mixin.rb
@@ -22,7 +22,9 @@ module NestedForm
       @template.after_nested_form(association) do
         model_object = object.class.reflect_on_association(association).klass.new
         output = %Q[<div id="#{association}_fields_blueprint" style="display: none">].html_safe
+        output.safe_concat('<tr class="fields"')
         output << fields_for(association, model_object, :child_index => "new_#{association}", &@fields[association])
+        output.safe_concat('</tr>')
         output.safe_concat('</div>')
         output
       end
